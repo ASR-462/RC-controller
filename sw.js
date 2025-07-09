83ad6946298b7dde1364f92c4c068a3c8926fc80
ASR-462 <sokilroma@gmail.com>
2025-07-09 15:53:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
-const CACHE_NAME = 'rc-controller-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png',
-    './icons/icon-512-maskable.png'
-];
-
+// Встановлення Service Worker
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+  console.log('Service Worker встановлюється');
+  event.waitUntil(
+    caches.open('rc-controller-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
 });
 
+// Активація Service Worker
+self.addEventListener('activate', event => {
+  console.log('Service Worker активовано');
+});
+
+// Обробка запитів
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
